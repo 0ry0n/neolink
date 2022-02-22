e3348e952c45dc43023e4e59dcba544d5ac371f1
0ry0n <newgsdeveloper@gmail.com>
2022-02-22 13:13:12 +0100

    Small fix... Now it works.

--- src/v4l/v4lt.rs
+++ src/v4l/v4lt.rs
@@ -61,21 +61,18 @@ impl V4lDevice {
             video_fps: None,
             video_format: None,
         };
-        result.apply_format();
         Ok(result)
     }
 
     pub(crate) fn run(&mut self) -> Result<()> {
-        let mut packets: usize = 0;
         // After we have created the device stream we cannot
         // edit the height/width etc
         // So first we pull packets from the camera until we have
         // enough data to setup the height etc
-        while (self.video_width.is_none()
+        while self.video_width.is_none()
             || self.video_height.is_none()
             || self.video_fps.is_none()
-            || self.video_format.is_none())
-            && packets <= 10
+            || self.video_format.is_none()
         {
             let media = self.receiver.recv()?;
             match media {
@@ -94,21 +91,23 @@ impl V4lDevice {
                     self.set_format(Some(video_type));
                 }
                 BcMedia::InfoV1(info) => {
-                    self.set_resolution(info.video_width, info.video_height);
-                    self.set_fps(info.fps);
+                    self.set_resolution(Some(info.video_width), Some(info.video_height));
+                    self.set_fps(Some(info.fps));
                 }
                 BcMedia::InfoV2(info) => {
-                    self.set_resolution(info.video_width, info.video_height);
-                    self.set_fps(info.fps);
+                    self.set_resolution(Some(info.video_width), Some(info.video_height));
+                    self.set_fps(Some(info.fps));
                 }
                 _ => {
                     //Ignore other BcMedia
                 }
             }
-            packets += 1;
         }
+        self.apply_format();
+
         // Now that we have fully determined the settings for the stream we can create the stream
         let mut stream = self.get_stream()?;
+        
         // Loop until error
         loop {
             let media = self.receiver.recv()?;
@@ -116,28 +115,26 @@ impl V4lDevice {
                 BcMedia::Iframe(payload) => {
                     let (buf_out, buf_out_meta) = OutputStream::next(&mut stream)?;
 
-                    // Output devices generally cannot know the exact size of the output buffers for
-                    // compressed formats (e.g. MJPG). They will however allocate a size that is always
-                    // large enough to hold images of the format in question. We know how big a buffer we need
-                    // since we control the input buffer - so just enforce that size on the output buffer.
                     let buf_out = &mut buf_out[0..payload.data.len()];
 
                     buf_out.copy_from_slice(&payload.data);
+                    buf_out_meta.bytesused = payload.data.len() as u32;
+                    //buf_out_meta.flags
                     buf_out_meta.field = 0;
-                    //buf_out_meta.bytesused = buf_in_meta.bytesused;
+                    //buf_out_meta.timestamp = Timestamp::new(0, payload.microseconds.into());
+                    //buf_out_meta.sequence
                 }
                 BcMedia::Pframe(payload) => {
                     let (buf_out, buf_out_meta) = OutputStream::next(&mut stream)?;
 
-                    // Output devices generally cannot know the exact size of the output buffers for
-                    // compressed formats (e.g. MJPG). They will however allocate a size that is always
-                    // large enough to hold images of the format in question. We know how big a buffer we need
-                    // since we control the input buffer - so just enforce that size on the output buffer.
                     let buf_out = &mut buf_out[0..payload.data.len()];
 
                     buf_out.copy_from_slice(&payload.data);
+                    buf_out_meta.bytesused = payload.data.len() as u32;
+                    //buf_out_meta.flags
                     buf_out_meta.field = 0;
-                    //buf_out_meta.bytesused = buf_in_meta.bytesused;
+                    //buf_out_meta.timestamp = Timestamp::new(0, payload.microseconds.into());
+                    //buf_out_meta.sequence
                 }
                 _ => {
                     //Ignore other BcMedia
@@ -152,33 +149,21 @@ impl V4lDevice {
     }
 
     fn set_format(&mut self, format: Option<StreamFormat>) {
-        match format {
-            Some(StreamFormat::H264) | Some(StreamFormat::H265) => {
-                if format != self.video_format {
-                    self.video_format = format;
-                    self.apply_format();
-                }
-            }
-            _ => {}
-        }
+        self.video_format = format;
     }
 
-    fn set_resolution(&mut self, width: u32, height: u32) {
-        self.video_width = Some(width);
-        self.video_height = Some(height);
-
-        self.apply_format();
+    fn set_resolution(&mut self, width: Option<u32>, height: Option<u32>) {
+        self.video_width = width;
+        self.video_height = height;
     }
 
-    fn set_fps(&mut self, fps: u8) {
-        self.video_fps = Some(fps);
-
-        self.apply_format();
+    fn set_fps(&mut self, fps: Option<u8>) {
+        self.video_fps = fps;
     }
 
     fn apply_format(&self) {
         let vid_format = match self.video_format {
-            Some(StreamFormat::H264) => b"AVC1",
+            Some(StreamFormat::H264) => b"H264",
             Some(StreamFormat::H265) => b"HEVC",
             None => return,
         };
@@ -192,13 +177,8 @@ impl V4lDevice {
 
             let params = Parameters::with_fps(self.video_fps.unwrap() as u32);
 
-            let sink_fmt = Output::set_format(&self.device, &fmt).unwrap();
-
-            println!("New out format:\n{}", sink_fmt);
-
-            let sink_params = Output::set_params(&self.device, &params);
-
-            println!("New out params:\n{}", sink_params.unwrap());
+            Output::set_format(&self.device, &fmt).unwrap();
+            Output::set_params(&self.device, &params).unwrap();
         }
     }
 }
